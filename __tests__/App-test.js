ffb18ec39ea03971f8c27b479b3ca6028dfe6089
Ho-0 <65712123+Ho-0@users.noreply.github.com>
2021-06-12 18:01:32 +0900

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,7 +9,12 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-describe('renders correctly', () => {
+//describe('renders correctly', () => {
   //renderer.create(<App />);
-  expect(1).toBe(1);
-});
+  //expect(1).toBe(1);
+//});
+
+describe('<Counter />', () => {
+  it('성공적으로 렌더링되어야 합니다.', () => {
+    expect(1).toBe(1);
+  });
