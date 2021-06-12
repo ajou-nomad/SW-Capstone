c88f77e2ebd035c2837f5a8511d615c283440121
Ho-0 <65712123+Ho-0@users.noreply.github.com>
2021-06-12 17:53:08 +0900

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,7 +9,7 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-test('renders correctly', () => {
+jest('renders correctly', () => {
   //renderer.create(<App />);
   expect(1).toBe(1);
 });
