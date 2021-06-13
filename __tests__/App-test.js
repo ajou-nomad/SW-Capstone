5f140c6968f1f5c50282d4436ac6451db6ac7add
Ho-0 <65712123+Ho-0@users.noreply.github.com>
2021-06-13 16:40:51 +0900

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,7 +7,7 @@ import React from 'react';
 //import App from '../App';
 
 // Note: test renderer must be required after react-native.
-//import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 //describe('renders correctly', () => {
   //renderer.create(<App />);
@@ -15,10 +15,10 @@ import React from 'react';
 //});
 
 
-//it('배수가 반환되어야 한다.', () => {
-//	const functionData = renderer.create(
-//		<Function />
-//	).getInstance()
-//
-//	expect(4).toEqual(4)
-//});
+it('배수가 반환되어야 한다.', () => {
+	const functionData = renderer.create(
+		<Function />
+	).getInstance()
+
+	expect(4).toEqual(4)
+});
