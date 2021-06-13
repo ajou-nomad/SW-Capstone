7b49565bdcb81d9dafff4d3aebe7b62d611a028f
Ho-0 <65712123+Ho-0@users.noreply.github.com>
2021-06-13 17:16:22 +0900

    Update App-test.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,7 +5,7 @@
 import 'react-native';
 import React from 'react';
 //import App from '../App';
-import messaging from '@react-native-firebase/messaging';
+//import messaging from '@react-native-firebase/messaging';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
